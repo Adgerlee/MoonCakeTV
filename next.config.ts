637f68a7f176b6924e2488bb375650cfaad930ad
Adger <112617391+Adgerlee@users.noreply.github.com>
2025-09-17 19:32:23 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: process.env.DOCKER_ENV === "true" ? "standalone" : undefined,
+  reactStrictMode: true, // 
 };
 
 export default nextConfig;
